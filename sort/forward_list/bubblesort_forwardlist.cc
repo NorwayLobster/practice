58f2f13f969f224515fd014a62d72e55f5cf4771
Cheng Wang <cheng.wang.801@gmail.com>
2020-12-03 13:44:48 +0800

    code bubblesort and not completed

--- sort/forward_list/bubblesort_forwardlist.cc
+++ sort/forward_list/bubblesort_forwardlist.cc
@@ -2,7 +2,7 @@
  * @Author: ChengWang(cheng.wang.801@gmail.com)
  * @Date: 2020-12-02 10:54:55
  * @LastEditors: ChengWang
- * @LastEditTime: 2020-12-03 04:27:45
+ * @LastEditTime: 2020-12-03 05:44:06
  * @FilePath: /practice/sort/forward_list/bubblesort_forwardlist.cc
  */
 #include "myforward_list.hpp"
@@ -11,17 +11,48 @@ bool bubble(Node* head,Node* sortedHead){
   dummy._next=head;
   Node* prev=&dummy;
   Node* cur=head;
+
+  if(!head->_next){//one node left
+  //头插法
+    head->_next=sortedHead->_next;//sortedHead is a dummy 
+    sortedHead->_next=head;
+    return true; 
+  }
+
   Node* next=head->_next;
-  while()
+  bool sorted;
+  while(next!=NULL){
+    sorted=true;
+    if(cur->_val > next->_val){
+      cur->_next=next->_next;
+      next->_next=cur;
+      prev->_next=next;
+
+      prev=prev->_next;
+      cur=prev->_next;
+      next=cur->_next;
+      sorted=false;
+    }
+    prev=cur;
+    cur=next;
+    next=next->_next;
+  }
+  prev->_next=NULL;//seperate the last node
+
+  //头插法
+  cur->_next=sortedHead->_next;//
+  sortedHead->_next=cur;
+  return sorted;
 }
+
 Node* bubblesort(Node* head){
   if(!head) return NULL;
   Node sorted;
   Node* sortedHead=&sorted;
   Node* cur=head;
-  while(cur!=NULL){
-      bool flag=bubble(head,sortedHead);
-      if(!flag) break;
+  while(cur!=NULL){//循环次数????
+      bool sortedflag=bubble(head,sortedHead);
+      if(sortedflag) break;
       cur=cur->_next;
   }
   return sorted._next;
