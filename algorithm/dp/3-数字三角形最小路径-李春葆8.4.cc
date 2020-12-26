348853326edda843c4ec70b6aa6e33359fe4aca4
ChengWang <cheng.wang.801@gmail.com>
2020-12-26 10:18:46 +0800

    3-数字三角形最小路径-李春葆8.4.cc

---
+++ algorithm/dp/3-数字三角形最小路径-李春葆8.4.cc
@@ -0,0 +1,23 @@
+/*
+ * @Author: ChengWang(cheng.wang.801@gmail.com)
+ * @Date: 2020-12-26 10:13:39
+ * @LastEditors: ChengWang
+ * @LastEditTime: 2020-12-26 10:18:21
+ * @FilePath: /practice/algorithm/dp/3-数字三角形最小路径-李春葆8.4.cc
+ */
+#include <iostream>
+#include <vector>
+int triangleMaxPathSum(vector<vector<int>>& arr){
+  int height=arr.size();
+  int width=arr[0].size();
+  vector<vector<int>> dp(height+1,vector<int>(width,0));
+//time, O(mn), space, O(mn) 
+//TODO
+//time, O(mn), space, O(2*width) : 滚动数组
+//time, O(mn), space, O(width) :upper, upper_left
+
+}
+int main(){
+
+  return 0;
+}
