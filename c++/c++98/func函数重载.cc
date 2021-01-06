6d2e2a670b1eae61a4463377a239bc8596830ba5
Cheng Wang <cheng.wang.801@gmail.com>
2021-01-06 22:28:54 +0800

    add interview 达观数据; rename singleton to sigleton

---
+++ c++/c++98/func函数重载.cc
@@ -0,0 +1,17 @@
+ ///
+ /// @file    func函数重载.cc
+ /// @author  ChengWANG(cheng.wang.801@gmail.com)
+ /// @date    2021-01-06 15:08:48
+ ///
+ 
+#include <iostream>
+using namespace std;
+int f(int a){
+	return 0;
+}
+void f(int a){
+	
+}
+int main(){
+	return 0;
+}
